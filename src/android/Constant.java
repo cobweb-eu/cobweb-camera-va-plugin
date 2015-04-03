1102af62d2865e9df15b2bf970fa32e65911c45a
Ruben Gamez <ruben.gamez@ed.ac.uk>
2015-04-03 16:06:42 +0100

    Add missing Constants.java from flood-app

---
+++ src/android/Constant.java
@@ -0,0 +1,80 @@
+package ie.ucd.cobweb;
+
+public class Constant {
+
+	public static final byte CHK = 0;
+	public static final byte CONF = 1;
+	public static final byte NW = 2;
+
+	public static final byte ZOOM = 17;
+
+	public static final String FIRSTU = "First";
+
+	public static final float STROKE_W = 6f;
+
+	public static final char OPTSEP = '#';
+	public static final String HMAP = "HMap";
+	public static final String LOGG = "Logg";
+	// Cobweb storage folder
+	public final static String SFOLDER = "COBWEB";
+
+	public final static String DFRMT = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
+	public final static String URL = "URL";
+	public final static String JSON = "JSON";
+	public final static String IMAGE = "IMAGE";
+
+	// Preference keys
+	public final static String NUMOBS = "NumObs";
+	public final static String POLYPR = "polygon";
+	public final static String UID = "UserID";
+	public final static String NAME = "Name";
+
+	public final static String OID = "OID";
+
+	public final static String PATH = "paths";
+	public final static String LK = "line_keys";
+	public final static String LV = "line_vals";
+	public final static String MK = "marker_keys";
+	public final static String MV = "marker_vals";
+	public final static String PK = "Pitch_keys";
+	public final static String PV = "Pitch_vals";
+
+	public final static String YK = "Yaw_keys";
+	public final static String YV = "Yaw_vals";
+	public final static String TK = "Time_Key";
+	public final static String TV = "Time_Val";
+
+	public final static String VK = "VA_Key";
+	public final static String VV = "VA_Val";
+	public final static String GK = "GPS_Key";
+	public final static String GV = "GPS_Val";
+
+	public final static String OYAW = "YAW";
+	public final static String OPITCH = "PITCH";
+	public final static String VA = "VA";
+	public final static String FLOC = "FileLocation";
+	public final static String COMPS = "COMPS";
+
+	public final static String CKY = "C_Key";
+	public final static String CVL = "C_Val";
+	public final static String ATMP = "AmTmp";
+	public final static String PRESS = "Pressure";
+	public final static String TMPK = "TmpK";
+	public final static String TMPV = "TmpV";
+	public final static String PRSK = "PrsK";
+	public final static String PRSV = "PrsV";
+
+	public final static double NOVAL = -1000;
+
+	public final static String MODE = "MODE";
+	public final static String OPTION = "OPTION";
+
+	public final static int NORMAL_PHOTO = 0;
+	public final static int PHOTO_M = 1;
+	public final static int PHOTO_P = 2;
+	public final static int PHOTO_P_M = 3;
+
+	public final static String MARKED = "MarkedArea";
+	public final static String POLYLINE = "Polyline";
+
+}
